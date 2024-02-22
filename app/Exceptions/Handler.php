47cd5c4e10e59d474c598ec90037716916a6cfc4
Sergi Majada <118728080+smajada@users.noreply.github.com>
2024-02-22 07:57:23 +0100

    formatear los errors

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,8 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Validation\ValidationException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -27,4 +29,39 @@ class Handler extends ExceptionHandler
             //
         });
     }
+
+    protected function invalidJson($request, ValidationException $exception): JsonResponse
+    {
+
+//        $errors = [];
+//        foreach ($exception->errors() as $field => $message) {
+//            $pointer = '/' . str_replace('.', '/', $field);
+//
+//            $errors[] = [
+//                'title' => $title,
+//                'detail' => $message[0],
+//                'source' => [
+//                    'pointer' => $pointer
+//                ]
+//            ];
+//        };
+
+
+        $title = $exception->getMessage();
+
+        return response()->json([
+            'errors' => collect($exception->errors())
+                ->map(function ($messages, $field) use ($title) {
+                    return [
+                        'title' => $title,
+                        'detail' => $messages[0],
+                        'source' => [
+                            'pointer' => "/" . str_replace('.', '/', $field)
+                        ]
+                    ];
+                })->values()
+        ], 422);
+    }
+
+
 }
--- tests/Feature/Articles/CreateArticleTest.php
+++ tests/Feature/Articles/CreateArticleTest.php
@@ -57,13 +57,27 @@ class CreateArticleTest extends TestCase
             'data' => [
                 'type' => 'articles',
                 'attributes' => [
+                    'title' => 'fd',
                     'slug' => 'nuevo-articulo',
                     'content' => 'Contenido del artículo'
                 ]
             ]
-        ]);
+        ])->dump();
 
-        $response->assertJsonValidationErrors('data.attributes.title');
+        $response->assertJsonStructure([
+            'errors' => [
+               ['title', 'detail', 'source' => ['pointer']]
+            ]
+        ])->assertJsonFragment([
+            'source' => [
+                'pointer' => '/data/attributes/title'
+            ]
+        ])->assertHeader(
+            'Content-Type',
+            'application/vnd.api+json'
+        )->assertStatus(422);
+
+//        $response->assertJsonValidationErrors('data.attributes.title');
 
     }
 
